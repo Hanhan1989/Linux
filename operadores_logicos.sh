d1cb06218eb0ce7147dd32ad03d39a35327ec395
Hanhan1989 <chenhanhan1989@gmail.com>
2019-01-18 22:40:55 +0100

    operadores logicos

---
+++ operadores_logicos.sh
@@ -0,0 +1,22 @@
+
+Compración de enteros
+
+-gt: mayor que
+-lt: menor que
+-ge: mayor que o igual a
+-le: menor que o igual a 
+-eq: igual a
+-ne: no es igual a
+
+Comparación de cadenas
+
+=: cadenas iguales
+!= cadenas diferentes
+Str: testea si la cadena no es nula
+-z prueba cadena vacía
+
+Operadores lógicos
+
+-a: and
+-o: Or 
+!: not 
