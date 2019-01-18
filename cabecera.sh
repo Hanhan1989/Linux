b94784818d502fb9dd697684a3005a77867000c0
Hanhan1989 <chenhanhan1989@gmail.com>
2019-01-18 11:13:01 +0100

    cabecera shellbang

---
+++ cabecera.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# La primera línea es "shebang". El shell siempre examina el primer carácter de un script para determinar de qué tipo es. 
+# Después de #! ponemos la ruta del programa de shell.
+
+echo "hola mundo"
+
