17bef3b6dac87345fc6c069c646cf9e3f1cc3f7d
Hanhan Chen <chenhanhan1989@gmail.com>
2020-06-13 09:48:03 +0200

    commandos útiles

---
+++ comandos_utiles.sh
@@ -0,0 +1,9 @@
+
+time [comando] : mide el tiempo de ejecución de un comando
+
+sudo httpry -i [interface red] : lista todos los requests exteriores 
+     ejemplo: sudo httpry -i enp0s8 | grep -i paypal
+
+head -n [numero] : mostrar x líneas empezando por el principio
+
+tail -n [numero] : mostrar x líneas empezando por el final  
