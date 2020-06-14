5b165379e8e7ece14326b0565b038391053d20da
Hanhan Chen <chenhanhan1989@gmail.com>
2020-06-14 17:57:53 +0200

    arrays

---
+++ array.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+nombres=(David Juan Maria Marta Jordi)
+
+# Print todos los índices del array
+echo ${nombres[@]}
+echo ${nombres[*]}
+
+#Print de un índice concreto del array
+echo ${nombres[2]}
+
+# Loop the array con for
+echo -e "\nLoop nombre \n";
+for nombre in ${nombres[@]}; do
+    echo $nombre
+done
+
+# Datos desde recursos
+ficheros=`ls .`
+for fichero in ${ficheros[@]}; do
+    echo $fichero
+done
