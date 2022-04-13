37654b2733cf01b639175df262799fc2fe057813
Hanhan1989 <chenhanhan1989@gmail.com>
2022-04-13 23:39:17 +0200

    [find] buscar todos los ficheros con la extension phtml

--- comandos_utiles.sh
+++ comandos_utiles.sh
@@ -43,3 +43,5 @@ inotifywait [path directory] --recursive --monitor : monitorear recursivamente l
 grep -R -B 1 -A 1 -i "[palabra a buscar]" [path directory] : buscar palabra con una línea por arriba y abajo de forma recursiva sin tener en cuenta las mayúsclas 
 
 httpry -i [interfaz red] : monitorear peticiones, respuestas, entrantes y salientes. Se puede saber la interfaz con ifconfig y nuestro ip del ordenador con hostname -I
+
+find [directory_name] -type f -name "*.phtml": buscar todos los ficheros con la extensión .phtml
